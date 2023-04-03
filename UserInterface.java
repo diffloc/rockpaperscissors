dfd084b819e206acac409ec346bb4f169598589b
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-03 09:09:50 -0500

    Stage 3/5 complete
    Loop game until "!exit"

--- UserInterface.java
+++ UserInterface.java
@@ -14,28 +14,35 @@ public class UserInterface {
 
     public void run(Scanner scanner) {
         inputPlayerGesture(scanner);
-        computer.generateComputerGesture();
-        validateWinner();
     }
 
     public void inputPlayerGesture(Scanner scanner) {
         // System.out.println("Available gestures: " + Gesture.getAvailableGestures());
         // System.out.print("Enter your gesture: ");
-        String playerGesture = scanner.nextLine();
-        try {
-            human.setGesture(Gesture.valueOf(playerGesture.toUpperCase()));
-        } catch (IllegalArgumentException e) {
-            System.out.printf("Invalid option: %s\n", playerGesture.equals("") ? "NULL" : playerGesture);
+        while (true) {
+            String playerGesture = scanner.nextLine();
+            if (playerGesture.equals(("!exit"))) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                try {
+                    human.setGesture(Gesture.valueOf(playerGesture.toUpperCase()));
+                } catch (IllegalArgumentException e) {
+                    System.out.print("Invalid input");
+                }
+                computer.generateComputerGesture();
+                validateWinner();
+            }
         }
     }
 
     public void validateWinner() {
         if (human.getGesture().equals(computer.getGesture())) {
-            System.out.printf("There is a draw (%s)", computer.getGesture().toString().toLowerCase());
+            System.out.printf("There is a draw (%s)\n", computer.getGesture().toString().toLowerCase());
         } else if (computer.getGesture().equals(human.getGesture().getOpposite())) {
-            System.out.printf("Sorry, but the computer chose %s", computer.getGesture().toString().toLowerCase());
+            System.out.printf("Sorry, but the computer chose %s\n", computer.getGesture().toString().toLowerCase());
         } else {
-            System.out.printf("Well done. The computer chose %s and failed", computer.getGesture().toString().toLowerCase());
+            System.out.printf("Well done. The computer chose %s and failed\n", computer.getGesture().toString().toLowerCase());
         }
     }
 }
